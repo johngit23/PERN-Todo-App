42a6c7e0a7c616e6b12a876773f77c80963b2f8a
Yohannes <jhon.klaus2008@gmail.com>
2024-04-07 17:38:01 +0300

    get, update and delete functionalities

--- server/server.js
+++ server/server.js
@@ -25,9 +25,52 @@ app.post("/todos", async (req, res) => {
   }
 });
 //Get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //Get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //Update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated!");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //Delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted!");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
